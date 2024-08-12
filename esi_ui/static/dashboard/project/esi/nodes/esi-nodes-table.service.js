bba0736cc70952810b3638cf1540167eaf186404
Anshul <anshulsaha15@gmail.com>
2024-08-12 18:44:56 +0000

    changes

--- esi_ui/static/dashboard/project/esi/nodes/esi-nodes-table.service.js
+++ esi_ui/static/dashboard/project/esi/nodes/esi-nodes-table.service.js
@@ -8,10 +8,11 @@
   nodesService.$inject = [
     '$uibModal',
     'horizon.framework.util.http.service',
-    'horizon.dashboard.project.esi.basePath',
+    'horizon.dashboard.project.esi.nodes.basePath',
+    'horizon.framework.widgets.toast.service',
   ];
   
-  function nodesService($uibModal, apiService, basePath) {
+  function nodesService($uibModal, apiService, basePath, toastService) {
     var service = {
       nodeList: nodeList,
       setPowerState: setPowerState,
@@ -24,7 +25,10 @@
     return service;
 
     function nodeList() {
-      return apiService.get('/api/esi/nodes/');
+      return apiService.get('/api/esi/nodes/').catch(err => {
+        toastService.add('error', 'Unable to retrieve ESI nodes. ' + (err.data ? err.data : ''))
+        return Promise.reject(err);
+      });
     }
 
     function setPowerState(node, target_state) {
@@ -32,27 +36,45 @@
         target: target_state
       };
 
-      return apiService.put('/api/esi/nodes/' + node.uuid + '/states/power', data);
+      return apiService.put('/api/esi/nodes/' + node.uuid + '/states/power', data).catch(err => {
+        toastService.add('error', 'Unable to set power state. ' + (err.data ? err.data : ''))
+        return Promise.reject(err);
+      });
     }
 
     function deleteLease(lease_uuid) {
-      return apiService.delete('/api/esi/lease/' + lease_uuid);
+      return apiService.delete('/api/esi/lease/' + lease_uuid).catch(err => {
+        toastService.add('error', 'Unable to delete lease(s). ' + (err.data ? err.data : ''))
+        return Promise.reject(err);
+      });
     }
 
     function provision(node, provision_params) {
-      return apiService.put('/api/esi/nodes/' + node.uuid + '/deploy/', provision_params);
+      return apiService.put('/api/esi/nodes/' + node.uuid + '/deploy/', provision_params).catch(err => {
+        toastService.add('error', 'Unable to provision a node. ' + (err.data ? err.data : ''))
+        return Promise.reject(err);
+      });
     }
 
     function unprovision(node) {
-      return apiService.put('/api/esi/nodes/' + node.uuid + '/undeploy/');
+      return apiService.put('/api/esi/nodes/' + node.uuid + '/undeploy/').catch(err => {
+        toastService.add('error', 'Unable to unprovision a node. ' + (err.data ? err.data : ''))
+        return Promise.reject(err);
+      });
     }    
 
     function networkAttach(node, network_params) {
-      return apiService.post('/api/esi/nodes/' + node.uuid + '/vifs', network_params);
+      return apiService.post('/api/esi/nodes/' + node.uuid + '/vifs', network_params).catch(err => {
+        toastService.add('error', 'Unable to attach network. ' + (err.data ? err.data : ''))
+        return Promise.reject(err);
+      });
     }
 
     function networkDetach(node, network_params) {
-      return apiService.delete('/api/esi/nodes/' + node.uuid + '/vifs/' + network_params.port);
+      return apiService.delete('/api/esi/nodes/' + node.uuid + '/vifs/' + network_params.port).catch(err => {
+        toastService.add('error', 'Unable to detach network. ' + (err.data ? err.data : ''))
+        return Promise.reject(err);
+      });
     }
   }
 
